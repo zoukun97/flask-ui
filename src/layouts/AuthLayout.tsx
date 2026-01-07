56388f2141812526baaa78e07948f06b3e4928f3
Shuzo Nakajima <zo.shu.0414.73@gmail.com>
2026-01-07 14:53:44 +0900

    authルーティング追加

---
+++ src/layouts/AuthLayout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router"
+
+export const AuthLayout = () => {
+  return (
+    <div>
+      <p>AuthLayout</p>
+      <Outlet />
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
+import { Regisuter } from './pages/auth/Register.tsx'
+import { Login } from './pages/auth/Login.tsx'
+import { AuthLayout } from './layouts/AuthLayout.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/auth/register" element={<Regisuter />} />
+          <Route path="/auth/login" element={<Login />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
