21d02961f2a1a0abab7aad7e1cccc0bdc274b75c
Shuzo Nakajima <zo.shu.0414.73@gmail.com>
2026-01-09 11:06:18 +0900

    ユーザー登録画面実装

---
+++ src/api/auth.ts
@@ -0,0 +1,23 @@
+import type { ApiResult, RegisterResponse } from "../types/api";
+
+export const register = async (formData: FormData): Promise<ApiResult<RegisterResponse>> => {
+  const url = 'http://127.0.0.1:5000/auth/register';
+  const res = await fetch(url, {
+    method: 'POST',
+    body: formData,
+  });
+
+  if (res.status !== 201) {
+    const data = await res.json();
+    return {
+      ok: false,
+      message: data.error
+    }
+  }
+  
+  const data: RegisterResponse = await res.json();
+  return {
+    ok: true,
+    data
+  }
+}
---
+++ src/types/api.ts
@@ -0,0 +1,16 @@
+export type ApiResult<T> =  ApiSuccess<T> | ApiError;
+
+type ApiSuccess<T> = {
+  ok: true,
+  data: T,
+}
+
+type ApiError = {
+  ok: false,
+  message: string,
+}
+
+export type RegisterResponse = {
+  id: number,
+  username: string,
+}
